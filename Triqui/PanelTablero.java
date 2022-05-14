0ca902d40dbc9754a3266f6a084b298c020c0701
felipenoguera <105526943+felipenoguera@users.noreply.github.com>
2022-05-13 19:56:49 -0500

    Create PanelTablero.java

---
+++ Triqui/PanelTablero.java
@@ -0,0 +1,15 @@
+package COM.USC.UI;
+import javax.swing.*;
+
+
+public class PanelTablero extends JPanel{
+    
+    JLabel etiquetaPrueba;
+    
+    public PanelTablero(){
+    
+        etiquetaPrueba=new JLabel("Aqui va el tablero");
+        this.add(etiquetaPrueba);
+   
+}
+}
