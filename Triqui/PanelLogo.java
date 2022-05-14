0acf1a8218bfa29d6b199058295bb6a484153ed9
felipenoguera <105526943+felipenoguera@users.noreply.github.com>
2022-05-13 19:56:08 -0500

    Create PanelLogo.java

---
+++ Triqui/PanelLogo.java
@@ -0,0 +1,15 @@
+package COM.USC.UI;
+
+import javax.swing.*;
+
+
+public class PanelLogo extends JPanel{
+ 
+    JLabel textologo;
+    
+    public PanelLogo(){
+  textologo = new JLabel("Aqui va el logo");
+  textologo.setText("Aqui va el logo");
+ add(textologo);
+    }
+}
